03d240469340cde0a630ab083efd28449370ed12
Ctyrone123 <Ctyrone123@gmail.com>
2020-06-25 11:30:45 -0500

    display issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,15 +8,21 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-          response.json().then(function(data) {
-            displayRepos(data, user);
-          });
-        } else {
-          alert("Error: " + response.statusText);
-        }
+    fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
       });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
+  });
   };
 
   var formSubmitHandler = function(event) {
@@ -33,20 +39,27 @@ var getUserRepos = function(user) {
     };
 
     var displayRepos = function(repos, searchTerm) {
+
+        // check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
         console.log(repos);
         console.log(searchTerm);
         // clear old content
         repoContainerEl.textContent = "";
         repoSearchTerm.textContent = searchTerm;
         // loop over repos
-    for (var i = 0; i < repos.length; i++) {
+    // loop over repos
+for (var i = 0; i < repos.length; i++) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
   
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
